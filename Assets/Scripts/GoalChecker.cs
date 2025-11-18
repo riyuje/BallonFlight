b9f90ff908c137175eaaaabb5f83c37a1fe7ab62
李悠ジェ <yuje0109@icloud.com>
2025-11-18 20:15:31 +0900

    手順20

---
+++ Assets/Scripts/GoalChecker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class GoalChecker : MonoBehaviour
+{
+    public float moveSpeed = 0.01f;  //移動速度
+
+    private float stopPos = 6.5f;  //停止地点。画面の右端でストップさせる
+
+    private bool isGoal;  //ゴールの重複判定防止用。一度ゴール判定したらtrueにして、ゴールの判定は1回だけしか行わないようにする
+    void Update()
+    {
+        //停止地点に到達するまで移動する
+        if(transform.position.x > stopPos)
+        {
+            transform.position += new Vector3(-moveSpeed, 0, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        //接触した(ゴールした)際に1回だけ判定する
+        if(col.gameObject.tag == "Player" && isGoal == false)
+        {
+            //2回目以降はゴール判定を行わないようにするために、trueに変更する
+            isGoal = true;
+
+            Debug.Log("ゲームクリア");
+        }
+    }
+}
